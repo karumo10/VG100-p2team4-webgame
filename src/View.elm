1e72db439c98217af5e73a0077327623e5c2dca4
yexiaosu <1714453105@qq.com>
2020-07-22 23:36:13 +0800

    [Developing] Enhancement #968 + Enhance the visualization of the suspects list.

--- src/View.elm
+++ src/View.elm
@@ -414,16 +414,24 @@ rendersuspectlist model =
         suspect =
             case model.map of
                 Park ->
-                    [ div [onClick Catherinecatch, style "opacity" (model.conclusion|>Debug.toString), class "btn btn-sm animated-button thar-three"] [text "Catherine"]
-                    , div [onClick Adkinscatch, style "opacity" (model.conclusion|>Debug.toString), class "btn btn-sm animated-button thar-three"] [text "Adkins"]
-                    , div [onClick Robbery, style "opacity" (model.conclusion|>Debug.toString), class "btn btn-sm animated-button thar-three"] [text "This is a robbery."]
+                    [ button [ onClick Catherinecatch
+                             , style "top" "100px", class "slide"
+                             , style "opacity" (model.conclusion|>Debug.toString)] [text "Catherine"]
+                    , button [ onClick Adkinscatch
+                             , style "top" "200px", class "slide"
+                             , style "opacity" (model.conclusion|>Debug.toString)] [text "Adkins"]
+                    , button [ onClick Robbery
+                             , style "top" "300px", class "slide"
+                             , style "opacity" (model.conclusion|>Debug.toString)] [text "This is a robbery."]
                     ]
                 _ ->
                     []
     in
-        [Html.input [Html.Attributes.type_ "checkbox", id "menu-toggle"][]
-        ,Html.label [Html.Attributes.for "menu-toggle", class "menu-icon"][Html.i [class "fa fa-bars"][]]
-        ,div [class "slideout-sidebar"] [div [class "container"] suspect]
+        [Html.input [Html.Attributes.type_ "checkbox", id "menu"][]
+        ,Html.label [Html.Attributes.for "menu", class "menu"][Html.span [] [], Html.span [] [], Html.span [] [] ]
+        ,Html.nav [class "nav"] [div [class "container"]
+                                     [div [ style "font-family" "Helvetica, Arial, sans-serif"
+                                          , style "font-size" "18px"] suspect]]
         ]
 
 renderBagButton : Model -> Html Msg
